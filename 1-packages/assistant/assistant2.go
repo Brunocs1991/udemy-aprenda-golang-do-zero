8b23e7bace7aec4f375d9e7bb2395c1af98b7e3f
Bruno Costa e Silva <brunocosta.1991@hotmail.com>
2025-04-27 13:38:33 -0300

    adjust name folder

--- 1-packages/assistant/assistant2.go
+++
@@ -1,7 +0,0 @@
-package assistant
-
-import "fmt"
-
-func write_2() {
-	fmt.Println("write from function write_2")
-}
--- 1-packages/go.mod
+++
@@ -1,5 +0,0 @@
-module module
-
-go 1.24.2
-
-require github.com/badoux/checkmail v1.2.4
